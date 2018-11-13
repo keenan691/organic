958f3895b1ee11f1a4a66f0ec19881ab633f99d5
Bartłomiej Nankiewicz <bartlomiej.nankiewicz@gmail.com>
2018-11-13 17:43:17 +0100

    Add typescript

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    "^.+\\.js?$": "babel-jest",
+    "^.+\\.ts?$": "babel-jest",
+  },
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node",
+  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!src/**/*.d.ts',
+  ],
+};
